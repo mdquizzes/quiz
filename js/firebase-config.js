725bcb1c2395f665d1bec9b194fdcb20557ee438
mdquizzes <perfectclasses@gmail.com>
2026-02-27 10:34:16 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,17 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
+import { initializeApp } from 
+"https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+
+import { getAuth } from 
+"https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+import { getFirestore } from 
+"https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCODdCPvN4L96ye0YuxippaIUHnEUwnT90",
+  apiKey: "PASTE FRESH ONE",
   authDomain: "mdquizzes-c255a.firebaseapp.com",
   projectId: "mdquizzes-c255a",
-  storageBucket: "mdquizzes-c255a.firebasestorage.app",
+  storageBucket: "mdquizzes-c255a.appspot.com",
   messagingSenderId: "510171455812",
   appId: "1:510171455812:web:dcead5207dc1272aa9b711"
 };
