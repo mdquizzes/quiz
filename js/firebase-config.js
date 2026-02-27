e9aa310e9615eaf0dc3e916f0b0d76ff65e7fe2b
mdquizzes <perfectclasses@gmail.com>
2026-02-27 11:37:00 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,3 +1,5 @@
+// 🔥 Firebase Config File
+
 import { initializeApp } from 
 "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 
@@ -7,16 +9,21 @@ import { getAuth } from
 import { getFirestore } from 
 "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
+
+// ✅ REAL FIREBASE CONFIG
 const firebaseConfig = {
   apiKey: "AIzaSyCODdCPvN4L96ye0YuxippaIUHnEUwnT90",
   authDomain: "mdquizzes-c255a.firebaseapp.com",
   projectId: "mdquizzes-c255a",
-  storageBucket: "mdquizzes-c255a.firebasestorage.app",
+  storageBucket: "mdquizzes-c255a.appspot.com",
   messagingSenderId: "510171455812",
   appId: "1:510171455812:web:dcead5207dc1272aa9b711"
 };
 
+
+// ✅ Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// ✅ Export services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
